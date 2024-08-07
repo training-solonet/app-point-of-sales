fd1f96a45ac24e85abeff73a7a3b0007eb7574db
edy-kurniawan <edy-kurniawan@users.noreply.github.com>
2024-08-07 02:29:33 +0000

    Fixes coding style

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\SatuanController;
-
 use App\Http\Controllers\JurnalPiutangController;
-use App\Http\Controllers\ReportPenjualanController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\ReportPembayaranPiutangController;
+use App\Http\Controllers\ReportPenjualanController;
+use App\Http\Controllers\SatuanController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('layouts.template');
