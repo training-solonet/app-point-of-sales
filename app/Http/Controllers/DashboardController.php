fc9675c49210027674c855875c983b2476f258a3
Elgiva Rasyad <141321900+Rasyaditya13@users.noreply.github.com>
2024-09-09 14:43:20 +0700

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,27 +18,29 @@ class DashboardController extends Controller
     {
         $invo = Jual::with(['det_jual.barang'])->find($id);
 
-        $data = [
-            'title' => 'Invoice Example',
-            'invoice' => $invo,
-        ];
+        $header = "-----------------------------\n"
+                .'DATE: '.now()->format('d-M-Y h:i:s A')."\n"
+                ."CASHIER: Admin\n"
+                .'-----------------------------';
 
-        $pdf = PDF::loadView('invoice.show', $data)
-            ->setPaper([0, 0, 216, 1000]);
-
-        $pdfPath = storage_path('app/public/invoice.pdf');
-
-        $pdf->save($pdfPath);
-
-        $parser = new Parser;
-        $pdfText = $parser->parseFile($pdfPath)->getText();
-
-        // Membuat instance PrintService
+        $items = [];
         $printService = new PrintService;
 
-        // Menggunakan service untuk mencetak
+        foreach ($invo->det_jual as $det) {
+            $itemLine = $printService->formatItemLine(
+                $det->barang->nama,
+                $det->qty,
+                number_format($det->harga_jual, 2),
+                number_format($det->qty * $det->harga_jual, 2)
+            );
+            $items[] = $itemLine;
+        }
 
-        $printService->printText($pdfText);
+        $totals = [];
+        $totals[] = $printService->formatTotalLine('Sub Total', number_format($invo->total, 2));
+        $totals[] = $printService->formatTotalLine('Discount', number_format($invo->discount, 2));
+
+        $printService->printReceipt('SoloNet', $header, $items, $totals);
 
         return response()->json(['status' => 'success']);
     }
